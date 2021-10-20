c305956c9d82eaf58ddf70e1cc72c29f87c47f7a
Kaszala Zoltán <zoltankaszala@gmail.com>
2021-10-20 23:21:19 +0200

    felhasználók tábla export

---
+++ forras/menu.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2021. Okt 20. 23:20
+-- Kiszolgáló verziója: 10.4.21-MariaDB
+-- PHP verzió: 7.4.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `felveteli`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `menu`
+--
+
+CREATE TABLE `menu` (
+  `url` varchar(30) NOT NULL,
+  `nev` varchar(30) NOT NULL,
+  `szulo` varchar(30) NOT NULL,
+  `jogosultsag` varchar(3) NOT NULL,
+  `sorrend` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- A tábla adatainak kiíratása `menu`
+--
+
+INSERT INTO `menu` (`url`, `nev`, `szulo`, `jogosultsag`, `sorrend`) VALUES
+('admin', 'Admin', '', '001', 80),
+('alapinfok', 'Alapinfók', 'elerhetoseg', '111', 40),
+('belepes', 'Belépés', '', '100', 60),
+('elerhetoseg', 'Elérhetőség', '', '111', 20),
+('kiegeszitesek', 'Kiegészítések', 'elerhetoseg', '011', 50),
+('kilepes', 'Kilépés', '', '011', 70),
+('linkek', 'Linkek', '', '100', 30),
+('nyitolap', 'Nyitólap', '', '111', 10),
+('regisztracio', 'Regisztráció', '', '100', 59);
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`url`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
