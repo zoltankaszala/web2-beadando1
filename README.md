4d5e701eef8d3502cafd77ee414a6c959307ef16
Kaszala Zoltán <zoltankaszala@gmail.com>
2021-10-29 10:23:34 +0200

    mnb

--- README.md
+++ README.md
@@ -6,25 +6,5 @@
 
 ### Hírek
 
-- `hirek/index`, `hirek/ervenytelen`,`hirek` - képzések listája
+- `hirek/index`, `hirek` - képzések listája
 - `hirek/1` - az 1-es kódú hír (ha van)
-
-
-### Képzések
-
-- `kepzes/index`, `kepzes/ervenytelen`,`kepzes` - képzések listája
-- `kepzes/mutat/1`, `kepzes/1` - az 1-es kódú képzés (ha van)
-
-### Jelentkezők
-
-- `jelentkezo/index`, `jelentkezo` - jelentkezők listája
-- `jelentkezo/1` - az 1-es kódú jelentkező (ha van)
-
-### Jelentkezések
-
-- `jelentkezes/index`, `jelentkezes` - jelentkezések listája
-- `jelentkezes/1` - az 1-es kódú jelentkezés (ha van)
-
-### Hibakezelés
-
-- `error/index`, `valami/ervenytelen` - 404-es hiba oldal 
--- controllers/router.php
+++ controllers/router.php
@@ -1,11 +1,11 @@
 <?php
 
 session_start();
-if(! isset($_SESSION['userid'])) $_SESSION['userid'] = 0;
-if(! isset($_SESSION['userlogin'])) $_SESSION['userlogin'] = "";
-if(! isset($_SESSION['userfirstname'])) $_SESSION['userfirstname'] = "";
-if(! isset($_SESSION['userlastname'])) $_SESSION['userlastname'] = "";
-if(! isset($_SESSION['userlevel'])) $_SESSION['userlevel'] = "1__";
+if (!isset($_SESSION['userid'])) $_SESSION['userid'] = 0;
+if (!isset($_SESSION['userlogin'])) $_SESSION['userlogin'] = "";
+if (!isset($_SESSION['userfirstname'])) $_SESSION['userfirstname'] = "";
+if (!isset($_SESSION['userlastname'])) $_SESSION['userlastname'] = "";
+if (!isset($_SESSION['userlevel'])) $_SESSION['userlevel'] = "1__";
 
 include(SERVER_ROOT . 'includes/database.inc.php');
 include(SERVER_ROOT . 'includes/menu.inc.php');
@@ -19,15 +19,14 @@ $vars = array();
 
 $request = $_SERVER['QUERY_STRING'];
 
-if($request != "")
-{
+if ($request != "") {
     $params = explode('/', $request);
     $page = array_shift($params); // a kért oldal neve
 
-    if(array_key_exists($page, Menu::$menu) && count($params)>0) // Az oldal egy menüpont oldala és van még adat az url-ben
+    if (array_key_exists($page, Menu::$menu) && count($params) > 0) // Az oldal egy menüpont oldala és van még adat az url-ben
     {
         $subpage = array_shift($params); // a kért aloldal
-        if(! (array_key_exists($subpage, Menu::$menu) && Menu::$menu[$subpage][1] == $page)) // ha nem egy alolal
+        if (!(array_key_exists($subpage, Menu::$menu) && Menu::$menu[$subpage][1] == $page)) // ha nem egy alolal
         {
             $vars[] = $subpage; // akkor ez egy parameter
             $subpage = ""; // és nincs aloldal
@@ -35,7 +34,7 @@ if($request != "")
     }
     $vars += $_POST;
 
-    foreach($params as $p) // a paraméterek tömbje feltöltése
+    foreach ($params as $p) // a paraméterek tömbje feltöltése
     {
         $vars[] = $p;
     }
@@ -45,29 +44,30 @@ if($request != "")
 // a fájlt és a hozzá tartozó vezérlõ oldalt is, akkor betöltjük az
 // elõbbiekben lekérdezett paramétereket továbbadva.
 
-$controllerfile = $page.($subpage != "" ? "_".$subpage : "");
-$target = SERVER_ROOT.'controllers/'.$controllerfile.'.php';
-if(! file_exists($target))
-{
+$controllerfile = $page . ($subpage != "" ? "_" . $subpage : "");
+$target = SERVER_ROOT . 'controllers/' . $controllerfile . '.php';
+if (!file_exists($target)) {
     $controllerfile = "error404";
-    $target = SERVER_ROOT.'controllers/error404.php';
+    $target = SERVER_ROOT . 'controllers/error404.php';
 }
 
 include_once($target);
-$class = ucfirst($controllerfile).'_Controller';
-if(class_exists($class))
-{ $controller = new $class; }
-else
-{ die('class does not exists!'); }
+$class = ucfirst($controllerfile) . '_Controller';
+if (class_exists($class)) {
+    $controller = new $class;
+} else {
+    die('class does not exists!');
+}
 
 // spl_autoload_register(...) függvény, amely ismeretlen osztály hívásakor, megpróbálja automatikusan betölteni a megfelelõ fájlt.
 // A modellekhez használjuk, egységesen nevezzük el fájljainkat (osztály nevével megegyezõ, csupa kisbetûs .php)
-spl_autoload_register(function($className) {
-    $file = SERVER_ROOT.'models/'.strtolower($className).'.php';
-    if(file_exists($file))
-    { include_once($file); }
-    else
-    { die("File '$filename' containing class '$className' not found.");    }
+spl_autoload_register(function ($className) {
+    $file = SERVER_ROOT . 'models/' . strtolower($className) . '.php';
+    if (file_exists($file)) {
+        include_once($file);
+    } else {
+        die("File '$file' containing class '$className' not found.");
+    }
 });
 
 $controller->main($vars);
